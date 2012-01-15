33035a02f047fdbb6062a9102cb98e4803d3a6ab
Matenia Rossides <matenia@gmail.com>
2012-01-15 15:34:04 +1100

    simplified asset path

--- lib/tinymce_fm/engine.rb
+++ lib/tinymce_fm/engine.rb
@@ -2,7 +2,7 @@ module TinymceFm
   class Engine < Rails::Engine
     #isolate_namespace TinymceFm
     initializer "add assets directories from pipeline" do |app|
-     app.config.assets.paths << "../../vendor/assets/javascripts"
+     app.config.assets.paths << "vendor/assets/javascripts"
    end
   end
 end
